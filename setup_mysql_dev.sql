5c8f2cfcc3831db4fd5a86336e954830fdd2f96a
derrickric <derrickmbaka@gmail.com>
2023-01-07 08:04:07 +0000

    minor changes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,4 @@
---Connect to the MySQL server as root
---creates a new user and a new database
+-- Creates a new user and a new database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
