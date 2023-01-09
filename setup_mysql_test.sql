3d2be17d5ac4a43823cb02ee4aea7ecad5d6e654
derrickric <derrickmbaka@gmail.com>
2023-01-09 16:00:45 +0000

    major changes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- Creates a database a new user with specified permissions
+--creates a database a new user with specified permissions
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 FLUSH PRIVILEGES;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
