66c59c49ec2d75a8a39877fedd8e1d8dbf27ab31
wizsenay <tomasfrehiwot@gmail.com>
2024-07-11 22:10:02 +0300

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
-
-from fabric.api import put, run, env
-from os.path import exists
-env.hosts = ['54.89.109.87', '100.25.190.21']
-
-
-def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-=======
 from fabric.api import put, run, local, env
 from os import path
 
@@ -61,5 +31,4 @@ def do_deploy(archive_path):
             .format(filename))
         return True
     except Exception as e:
->>>>>>> f9c02ad0c79ed4eb2f37d23e08a8fae6f9fe792e
         return False
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,29 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
-
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
-
-from fabric.api import env, local, put, run
-from datetime import datetime
-from os.path import exists, isdir
-env.hosts = ['54.160.77.90', '10.25.190.21']
-
-
-def do_pack():
-    """generates a tgz archive"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
-=======
 from fabric.api import put, run, local, env
 from time import strftime
 from datetime import date
@@ -44,30 +19,10 @@ def do_pack():
         return "versions/web_static_{}.tgz".format(filename)
 
     except Exception as e:
->>>>>>> f9c02ad0c79ed4eb2f37d23e08a8fae6f9fe792e
         return None
 
 
 def do_deploy(archive_path):
-<<<<<<< HEAD
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
-        return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
-=======
     """Fabric script that distributes
     an archive to your web server"""
 
@@ -92,18 +47,10 @@ def do_deploy(archive_path):
             .format(filename))
         return True
     except Exception as e:
->>>>>>> f9c02ad0c79ed4eb2f37d23e08a8fae6f9fe792e
         return False
 
 
 def deploy():
-<<<<<<< HEAD
-    """creates and distributes an archive to the web servers"""
-    archive_path = do_pack()
-    if archive_path is None:
-        return False
-    return do_deploy(archive_path)
-=======
     """run the 2 functions"""
 
     path = do_pack()
@@ -111,4 +58,3 @@ def deploy():
         return False
 
     return do_deploy(path)
->>>>>>> f9c02ad0c79ed4eb2f37d23e08a8fae6f9fe792e
